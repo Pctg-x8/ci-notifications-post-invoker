9d55c2f4094f6d44f8e0a3b92f66a150235f326e
S.Percentage <Syn.Tri.Naga@gmail.com>
2021-08-12 00:26:58 +0900

    fix: import github package

--- src/Actions/Context.js
+++ src/Actions/Context.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const github = require("@actions/github");
+
 exports.repoOwner = github.context.repo.owner;
 exports.repoName = github.context.repo.repo;
 exports.runId = github.context.runId;
