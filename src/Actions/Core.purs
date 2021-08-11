ad385af75c547072a13653999c35803b036f46ba
S.Percentage <Syn.Tri.Naga@gmail.com>
2021-08-12 00:33:58 +0900

    fix: upper conversion for input variable name

--- src/Actions/Core.purs
+++ src/Actions/Core.purs
@@ -3,6 +3,7 @@ module Actions.Core where
 import Prelude
 
 import Data.Maybe (Maybe, fromJust, maybe)
+import Data.String (toUpper)
 import Effect (Effect)
 import Effect.Exception (throw)
 import Node.Path (FilePath)
@@ -16,7 +17,7 @@ headRef :: Effect (Maybe String)
 headRef = lookupEnv "GITHUB_HEAD_REF"
 
 getInput :: String -> Effect (Maybe String)
-getInput name = lookupEnv $ "INPUT_" <> name
+getInput name = lookupEnv $ "INPUT_" <> toUpper name
 
 getRequiredInput :: String -> Effect String
 getRequiredInput name = getInput name >>= maybe (throw $ "Input required and not supplied: " <> name) pure
